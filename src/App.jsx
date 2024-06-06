1e6823cec97c2d63b3389ae611c01dc9ea6273c2
Adi Booker <adib4324@gmail.com>
2024-06-06 16:58:04 +0300

    back to old .env method

--- src/App.jsx
+++ src/App.jsx
@@ -35,3 +35,41 @@ function App() {
 }
 
 export default App;
+
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Sidebar from "./components/Sidebar";
+// import Articles from "./components/Articles";
+// import SignUp from "./components/SignUp";
+// import SignIn from "./components/SignIn";
+// import CreateArticle from "./components/CreateArticle";
+// import ArticleDetail from "./components/ArticleDetails";
+// import CategoryPage from "./components/CategoryPage";
+// import UserProfile from "./components/UserProfile";
+// import "./scss/main.scss";
+
+// function App() {
+// 	return (
+// 		<div className="app">
+// 			<Navbar />
+// 			<Sidebar />
+// 			<div
+// 				className="main-content"
+// 				style={{ marginLeft: "240px", marginTop: "64px", padding: "20px" }}
+// 			>
+// 				<Routes>
+// 					<Route path="/" element={<Articles />} />
+// 					<Route path="/signup" element={<SignUp />} />
+// 					<Route path="/signin" element={<SignIn />} />
+// 					<Route path="/create-article" element={<CreateArticle />} />
+// 					<Route path="/articles/:id" element={<ArticleDetail />} />
+// 					<Route path="/category/:category" element={<CategoryPage />} />
+// 					<Route path="/profile" element={<UserProfile />} />
+// 				</Routes>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export default App;
--- src/api/axios.js
+++ src/api/axios.js
@@ -6,3 +6,11 @@ const instance = axios.create({
 });
 
 export default instance;
+// import axios from "axios";
+
+// const instance = axios.create({
+// 	baseURL: process.env.REACT_APP_API_BASE_URL,
+// 	withCredentials: true,
+// });
+
+// export default instance;
