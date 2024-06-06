24410fbb2214587fc922bba20d7a0323049c8a3c
Adi Booker <adib4324@gmail.com>
2024-06-06 16:32:30 +0300

    fix2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,41 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
-import { fetchCategories } from "../api/api";
-import "../scss/components/_sidebar.scss";
-
-// Icons for different categories
 import ScienceIcon from "@mui/icons-material/Science";
 import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
-import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
-import MovieIcon from "@mui/icons-material/Movie";
-
-const categoryIcons = {
-	technology: <ScienceIcon />,
-	health: <HealthAndSafetyIcon />,
-	finance: <MonetizationOnIcon />,
-	sports: <SportsSoccerIcon />,
-	entertainment: <MovieIcon />,
-};
+import "../scss/components/_sidebar.scss";
 
 const Sidebar = () => {
-	const [categories, setCategories] = useState([]);
-
-	useEffect(() => {
-		const fetchAllCategories = async () => {
-			try {
-				const categories = await fetchCategories();
-				setCategories(categories);
-			} catch (error) {
-				console.error("Error fetching categories:", error);
-			}
-		};
-
-		fetchAllCategories();
-	}, []);
-
 	return (
 		<div className="sidebar">
 			<img src="/path-to-your-logo.png" alt="Logo" className="sidebar__logo" />
@@ -51,22 +23,39 @@ const Sidebar = () => {
 					</ListItemIcon>
 					<ListItemText primary="Home" />
 				</ListItem>
-				{categories.map((category) => (
-					<ListItem
-						button
-						component={NavLink}
-						to={`/category/${category}`}
-						className="sidebar__menu__item"
-						key={category}
-					>
-						<ListItemIcon>
-							{categoryIcons[category.toLowerCase()] || <HomeIcon />}
-						</ListItemIcon>
-						<ListItemText
-							primary={category.charAt(0).toUpperCase() + category.slice(1)}
-						/>
-					</ListItem>
-				))}
+				<ListItem
+					button
+					component={NavLink}
+					to="/category/technology"
+					className="sidebar__menu__item"
+				>
+					<ListItemIcon>
+						<ScienceIcon />
+					</ListItemIcon>
+					<ListItemText primary="Technology" />
+				</ListItem>
+				<ListItem
+					button
+					component={NavLink}
+					to="/category/health"
+					className="sidebar__menu__item"
+				>
+					<ListItemIcon>
+						<HealthAndSafetyIcon />
+					</ListItemIcon>
+					<ListItemText primary="Health" />
+				</ListItem>
+				<ListItem
+					button
+					component={NavLink}
+					to="/category/finance"
+					className="sidebar__menu__item"
+				>
+					<ListItemIcon>
+						<MonetizationOnIcon />
+					</ListItemIcon>
+					<ListItemText primary="Finance" />
+				</ListItem>
 			</List>
 		</div>
 	);
